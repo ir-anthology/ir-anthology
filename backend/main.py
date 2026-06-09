9ce6bb7c128c1d3a982395affa8355c28e6b1aac
Ben Gerhards <ben.gerhards@uni-kassel.de>
2026-06-09 09:33:00 +0200

    add backend

---
+++ backend/main.py
@@ -0,0 +1,174 @@
+from fastapi import FastAPI, HTTPException, Query, Request, Depends
+from typing import Annotated
+from pydantic import BaseModel
+from enum import Enum
+import sparqlTemplates
+import httpx
+from contextlib import asynccontextmanager
+
+SPARQL_ENDPOINT = "http://database.ir-anthology.srv.webis.de/"
+
+VALID_ENTITIES = ['Author', 'Venue', 'Publication', 'Year', '2020s', '2010s', '2000s', 'Pre2000s']
+
+class Entities(str, Enum):
+    author = "Author"
+    venue = "Venue"
+    publication = "Publication"
+    year = "Year"
+    twenty_twenties = "2020s"
+    twenty_tens = "2010s"
+    two_thousands = "2000s"
+    pre_two_thousands = "Pre2000s"
+
+class FilterParams(BaseModel):
+    entity: Entities | None = "Author"
+    sort_by: str | None = "Publication"
+    order: str | None = "DESC"
+    page: int | None = 1
+    limit: int | None = 50
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.client = httpx.AsyncClient(timeout=60.0)
+    yield
+    await app.state.client.aclose()
+
+def get_client(request: Request) -> httpx.AsyncClient:
+    return request.app.state.client
+
+def get_uri_from_id(ir_id: str):
+    return "https://dblp.org/" + ir_id.replace("+", "/")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/api/table")
+async def read_table_data(filter_query: Annotated[FilterParams, Query()], client: httpx.AsyncClient = Depends(get_client), *, request: Request):
+    extra = {
+        k: v for k, v in request.query_params.items()
+        if k not in filter_query.model_dump().keys()
+    }
+    order_clause = parse_order(filter_query.sort_by, filter_query.order)
+    offset = (filter_query.page - 1) * filter_query.limit
+    filters = build_filters(extra)
+    query = sparqlTemplates.TABLE_QUERY_TEMPLATE.replace("$ENTITY_TYPE", filter_query.entity)
+    query = query.replace('$FILTERS', filters)
+    query = query.replace('$ORDER', order_clause)
+    query = query.replace('$LIMIT', str(filter_query.limit))
+    query = query.replace('$OFFSET', str(offset))
+    data = await sparql_post(query, client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/anthology")
+async def read_anthology(client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.ANTHOLOGY_QUERY_TEMPLATE, client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/conferences/{id}")
+async def read_conference(id: str, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.VENUE_PROCEEDINGS_TEMPLATE.replace('$VENUE_URI', get_uri_from_id(id)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/conferences/{id}/{year}/inproceedings")
+async def read_conference(id: str, year: int, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.INPROCEEDINGS_FROM_PROCEEDINGS_TEMPLATE.replace('$VENUE_ID', get_uri_from_id(id)).replace('$YEAR', str(year)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/conferences/{id}/{year}/proceedings")
+async def read_conference_year(id: str, year: int, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.PROCEEDINGS_QUERY_TEMPLATE.replace('$VENUE_ID', get_uri_from_id(id)).replace('$YEAR', str(year)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/journals/{id}")
+async def read_journal(id: str, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.JOURNAL_OVERVIEW_TEMPLATE.replace('$JOURNAL', get_uri_from_id(id)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/journals/{id}/{year}")
+async def read_journal_year(id: str, year: int, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.ARTICLES_FROM_JOURNAL_TEMPLATE.replace('$JOURNAL', get_uri_from_id(id)).replace('$YEAR', str(year)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/people/{id}")
+async def read_venue(id: str, client: httpx.AsyncClient = Depends(get_client)):
+    data = await sparql_post(sparqlTemplates.PERSON_TEMPLATE.replace('$AUTHOR', get_uri_from_id(id)), client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+@app.get("/api/publications/{id}")
+async def read_venue(id: str, client: httpx.AsyncClient = Depends(get_client)):
+    query = sparqlTemplates.BIB_PUBLICATION_TEMPLATE.replace('$PUBLICATION', get_uri_from_id(id))
+    print(query)
+    data = await sparql_post(query, client)
+    return {"vars": data["head"]["vars"], "bindings": data["results"]["bindings"]}
+
+def parse_order(sort_by: str | None, order: str) -> str:
+    if sort_by is None:
+        return 'ORDER BY DESC(?Publication)'
+    
+    direction = "ASC" if order == 'ASC' else 'DESC'
+    return f"ORDER BY {direction}(?{sort_by})"
+
+def get_label_var(entity_type: str) -> str:
+    return f"?{entity_type.lower()}_label"
+
+def get_uri_var(entity_type: str) -> str:
+    return f"?{entity_type.lower()}_URI"
+
+def build_filters(search_params: dict[str, str], filter_mode = 'label') -> str :
+    filters: dict[str, list[str]] = {}
+    for key in search_params:
+        if not key.startswith('filter_'):
+            continue
+        value = search_params[key];
+        if value is None:
+            continue
+        filters[key[len('filter_'):]] = value.split(',')
+
+    if len(filters) == 0:
+        return ''
+
+    filter_clauses: list[str] = []
+    for key in filters:
+        values = filters[key]
+        if  len(values) == 0:
+            continue;
+        if filter_mode == 'uri':
+            uri_var = get_uri_var(key)
+            uri_values = []
+            for v in values:
+                if v.startswith("http://") or v.startswith("https://"):
+                    uri_values.append(v)
+            if len(uri_values) > 0:
+                in_string = f"<{uri_values[0]}>"
+                for i in range(1, len(uri_values)):
+                    in_string+= f",<{uri_values[i]}>"
+                filter_clauses.append(f"FILTER({uri_var} IN ({in_string}))")
+        else :
+            label_var = get_label_var(key)
+            contains_clauses = []
+            for v in values:
+                contains_clauses.append(f'CONTAINS(LCASE({label_var}), LCASE("{v.lower().replace("'", "\\'")}"))')
+            contains_string = f"{contains_clauses[0]}"
+            for i in range(1, len(contains_clauses)):
+                contains_string +=  f" || {contains_clauses[i]}"
+            print(contains_string)
+            filter_clauses.append(f"FILTER({contains_string})")
+
+    print(filter_clauses)
+    filter_string = f"{filter_clauses[0]}"
+    for i in range(1, len(filter_clauses)):
+        filter_string += f"\n  {filter_clauses[i]}"
+    print(filter_string)
+    return filter_string;
+
+async def sparql_post(query: str, client: httpx.AsyncClient) -> dict:
+    response = await client.post(
+        SPARQL_ENDPOINT,
+        headers={
+            "Accept": "application/sparql-results+json",
+            "Content-Type": "application/sparql-query",
+        },
+        content=query,
+    )
+    if not response.is_success:
+        raise HTTPException(response.status_code, "Could not get resource from database")
+    return response.json()
---
+++ backend/sparqlTemplates.py
@@ -0,0 +1,377 @@
+TABLE_QUERY_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX dcterms: <http://purl.org/dc/terms/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd:  <http://www.w3.org/2001/XMLSchema#>
+
+SELECT (?entity_label AS ?Entity)
+  (?entity_URI AS ?URI)
+  (COUNT(DISTINCT ?publication_label) AS ?Publication)
+  (COUNT(DISTINCT ?venue_label)       AS ?Venue)
+  (COUNT(DISTINCT ?author_label)      AS ?Author)
+  (COUNT(DISTINCT ?year_label)        AS ?Year)
+  (COUNT(DISTINCT ?2020s_label)       AS ?2020s)
+  (COUNT(DISTINCT ?2010s_label)       AS ?2010s)
+  (COUNT(DISTINCT ?2000s_label)       AS ?2000s)
+  (COUNT(DISTINCT ?Pre2000s_label)    AS ?Pre2000s)
+WHERE {
+  VALUES ?entityType { "$ENTITY_TYPE" }
+  ?publication_URI dblp:title ?publication_label ;
+                   dblp:yearOfPublication ?year_label ;
+                   dblp:authoredBy ?author_URI ;
+                   dblp:publishedInStream ?venue_URI .
+
+  OPTIONAL {
+    ?venue_URI dblp:primaryStreamTitle ?venue_label .
+  }
+  OPTIONAL {
+    ?author_URI dblp:primaryCreatorName ?author_label .
+  }
+  
+  BIND(xsd:integer(STR(?year_label)) AS ?y)
+  
+  BIND(IF(?y >= 2020, ?year_label, ?unbound) AS ?2020s_label)
+  BIND(IF(?y >= 2010 && ?y < 2020, ?year_label, ?unbound) AS ?2010s_label)
+  BIND(IF(?y >= 2000 && ?y < 2010, ?year_label, ?unbound) AS ?2000s_label)
+  BIND(IF(?y < 2000, ?year_label, ?unbound) AS ?Pre2000s_label)
+  
+  $FILTERS
+
+  BIND(
+    IF(?entityType = "Publication", ?publication_URI,
+    IF(?entityType = "Venue",       ?venue_URI,
+    IF(?entityType = "Year",        ?year_URI,
+    IF(?entityType = "2020s",       ?2020s_URI,
+    IF(?entityType = "2010s",       ?2010s_URI,
+    IF(?entityType = "2000s",       ?2000s_URI,
+    IF(?entityType = "Pre2000s",    ?Pre2000s_URI,
+                                    ?author_URI)))))))
+  AS ?entity_URI)
+  BIND(
+    IF(?entityType = "Publication", ?publication_label,
+    IF(?entityType = "Venue",       ?venue_label,
+    IF(?entityType = "Year",        ?year_label,
+    IF(?entityType = "2020s",       ?2020s_label,
+    IF(?entityType = "2010s",       ?2010s_label,
+    IF(?entityType = "2000s",       ?2000s_label,
+    IF(?entityType = "Pre2000s",    ?Pre2000s_label,
+                                    ?author_label)))))))
+  AS ?entity_label)
+
+}
+GROUP BY ?entity_label ?entity_URI
+HAVING (BOUND(?entity_label) && BOUND(?entity_URI))
+$ORDER
+LIMIT $LIMIT
+OFFSET $OFFSET
+'''
+
+# With the new scheme we can also distinguish between journal and conference
+ANTHOLOGY_QUERY_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+
+SELECT DISTINCT ?stream ?venue_label ?year_label ?type
+WHERE {
+  VALUES ?stream {
+      <https://dblp.org/streams/journals/ftir>
+      <https://dblp.org/streams/journals/ijirr>
+      <https://dblp.org/streams/journals/ijmir>
+      <https://dblp.org/streams/journals/ipm>
+      <https://dblp.org/streams/journals/ir>
+      <https://dblp.org/streams/journals/jasis>
+      <https://dblp.org/streams/journals/sigir>
+      <https://dblp.org/streams/journals/tismir>
+      <https://dblp.org/streams/journals/tist>
+      <https://dblp.org/streams/journals/tois>
+      <https://dblp.org/streams/journals/tweb>
+      <https://dblp.org/streams/journals/wwwj>
+      <https://dblp.org/streams/conf/adcs>
+      <https://dblp.org/streams/conf/airs>
+      <https://dblp.org/streams/conf/ccir>
+      <https://dblp.org/streams/conf/ceri>
+      <https://dblp.org/streams/conf/chiir>
+      <https://dblp.org/streams/conf/cikm>
+      <https://dblp.org/streams/conf/civr>
+      <https://dblp.org/streams/conf/clef>
+      <https://dblp.org/streams/conf/coria>
+      <https://dblp.org/streams/conf/desires>
+      <https://dblp.org/streams/conf/dir>
+      <https://dblp.org/streams/conf/ecir>
+      <https://dblp.org/streams/conf/fdia>
+      <https://dblp.org/streams/conf/fire>
+      <https://dblp.org/streams/conf/ictir>
+      <https://dblp.org/streams/conf/irfc>
+      <https://dblp.org/streams/conf/ismir>
+      <https://dblp.org/streams/conf/mir>
+      <https://dblp.org/streams/conf/ntcir>
+      <https://dblp.org/streams/conf/sigir>
+      <https://dblp.org/streams/conf/spire>
+      <https://dblp.org/streams/conf/trec>
+      <https://dblp.org/streams/conf/wsdm>
+      <https://dblp.org/streams/conf/www>
+  }
+  ?publication_URI dblp:yearOfPublication ?year_label ;
+                   dblp:publishedInStream ?stream .
+
+  ?stream a ?type ;
+          dblp:primaryStreamTitle ?venue_label .
+  FILTER(?type != dblp:Stream)
+}
+ORDER BY ?type ?venue_label ?year_label 
+'''
+
+YEARS_QUERY_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT DISTINCT ?year_label ?title ?streamTitle ?pub WHERE{
+VALUES ?stream {
+<$VENUE_URI>
+}
+?pub dblp:yearOfPublication ?year_label ;
+                 dblp:publishedInStream ?stream ;
+                 dblp:title ?title ;
+                 dblp:bibtexType bibtex:Proceedings .
+?stream dblp:primaryStreamTitle ?streamTitle .
+}
+'''
+'''
+const VENUE_PROCEEDINGS_TEMPLATE = `
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?year_label ?title ?streamTitle ?pub (COUNT(DISTINCT ?paper) AS ?count) WHERE {
+  {
+    SELECT DISTINCT ?stream WHERE{
+      { BIND(<$VENUE_URI> AS ?stream) }
+      UNION
+      { ?stream dblp:superStream <$VENUE_URI> .}
+      UNION
+      { <$VENUE_URI> dblp:subStream ?stream .}
+    }
+  }
+  ?pub dblp:yearOfPublication ?year_label ;
+       dblp:publishedInStream ?stream ;
+       dblp:title ?title ;
+       dblp:bibtexType bibtex:Proceedings .
+  <$VENUE_URI> dblp:primaryStreamTitle ?streamTitle .
+  OPTIONAL {
+    ?paper dblp:publishedAsPartOf ?pub ;
+           dblp:bibtexType bibtex:Inproceedings .
+  }
+}
+GROUP BY ?year_label ?title ?streamTitle ?pub
+ORDER BY DESC(?year_label) ?title
+'''
+
+VENUE_PROCEEDINGS_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?year_label ?title ?streamTitle ?pub (COUNT(DISTINCT ?paper) AS ?count) WHERE {
+  VALUES ?stream {
+    <$VENUE_URI>
+  }
+  ?pub dblp:yearOfPublication ?year_label ;
+       dblp:publishedInStream ?stream ;
+       dblp:title ?title ;
+       dblp:bibtexType bibtex:Proceedings .
+  ?stream dblp:primaryStreamTitle ?streamTitle .
+  OPTIONAL {
+    ?paper dblp:publishedAsPartOf ?pub ;
+           dblp:bibtexType bibtex:Inproceedings .
+  }
+}
+GROUP BY ?year_label ?title ?streamTitle ?pub
+ORDER BY DESC(?year_label) ?title
+'''
+
+PROCEEDINGS_QUERY_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?title ?doi ?pub ?streamTitle WHERE{
+  VALUES ?stream {
+    <$VENUE_ID>
+  }
+  ?pub dblp:title ?title ;
+       dblp:publishedInStream ?stream ;
+	   dblp:bibtexType bibtex:Proceedings ;
+	   dblp:yearOfPublication ?year .
+  ?stream dblp:primaryStreamTitle ?streamTitle .
+  FILTER(STR(?year) = "$YEAR")
+
+  OPTIONAL{?pub dblp:doi ?doi}
+}
+GROUP BY ?title ?doi ?pub ?streamTitle
+'''
+
+INPROCEEDINGS_FROM_PROCEEDINGS_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?title ?doi ?book ?pub
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", ?authorName); separator=", ") AS ?authors)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", STR(?authorUri)); separator=", ") AS ?authorIds)
+WHERE{
+  VALUES ?stream {
+    <$VENUE_ID>
+  }
+  ?book dblp:publishedInStream ?stream ;
+        dblp:bibtexType bibtex:Proceedings ;
+        dblp:yearOfPublication ?year .
+  FILTER(STR(?year) = "$YEAR")
+
+  ?pub dblp:publishedAsPartOf ?book ;
+       dblp:title ?title ;
+	   dblp:bibtexType bibtex:Inproceedings .
+
+  OPTIONAL{?pub dblp:doi ?doi}
+  OPTIONAL {
+    ?pub dblp:hasSignature ?sig .
+    ?sig a dblp:AuthorSignature ;
+         dblp:signatureOrdinal ?ord ;
+         dblp:signatureCreator ?authorUri ;
+         dblp:signatureDblpName ?authorName .
+  }
+}
+GROUP BY ?title ?doi ?book ?pub
+'''
+
+BIB_PUBLICATION_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?title ?booktitle ?series ?pages ?publisher ?doi ?url ?year ?book ?pub ?stream ?streamTitle ?month ?volume ?number ?isbn ?bibtexType
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", ?authorName); separator=", ") AS ?authors)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", STR(?authorUri)); separator=", ") AS ?authorIds)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?edOrd), "@@", ?editorName); separator=", ") AS ?editors)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?edOrd), "@@", STR(?editor)); separator=", ") AS ?editorIds)
+WHERE{
+  VALUES ?pub {
+    <$PUBLICATION>
+  }
+  ?pub dblp:title ?title ;
+       dblp:publishedInStream ?stream ;
+	   dblp:bibtexType ?bibtexType ;
+	   dblp:yearOfPublication ?year .
+
+  ?stream dblp:primaryStreamTitle ?streamTitle .
+
+  OPTIONAL{?pub dblp:publishedInSeries ?series}
+  OPTIONAL{?pub dblp:pagination ?pages}
+  OPTIONAL{?pub dblp:doi ?doi}
+  OPTIONAL{?pub dblp:publishedBy ?publisher}
+  OPTIONAL{?pub dblp:primaryDocumentPage ?url}
+  OPTIONAL{?pub dblp:monthOfPublication ?month}
+  OPTIONAL{?pub dblp:publishedInJournalVolume ?volume}
+  OPTIONAL{?pub dblp:publishedInJournalVolumeIssue ?number}
+  OPTIONAL{?pub dblp:isbn ?isbn}
+  OPTIONAL{?pub dblp:hasSignature ?sig .
+            ?sig a dblp:AuthorSignature ;
+                 dblp:signatureOrdinal ?ord ;
+                 dblp:signatureCreator ?authorUri ;
+                 dblp:signatureDblpName ?authorName}
+  OPTIONAL {?pub dblp:publishedAsPartOf ?book .
+            ?book dblp:title ?booktitle .
+            OPTIONAL {
+                ?book dblp:editedBy ?editor .
+                ?editor dblp:primaryCreatorName ?editorName .
+                OPTIONAL {
+                    ?book dblp:hasSignature ?bookEdSig .
+                    ?bookEdSig dblp:signatureCreator ?editor ;
+                               dblp:signatureOrdinal ?edOrd .
+                }
+            }
+            OPTIONAL { ?book dblp:publishedBy ?publisher }
+            OPTIONAL { ?book dblp:publishedInSeries ?series }
+  }
+
+}
+GROUP BY ?title ?booktitle ?series ?pages ?publisher ?doi ?url ?year ?book ?pub ?stream ?streamTitle ?month ?volume ?number ?isbn ?bibtexType
+'''
+
+ARTICLES_FROM_JOURNAL_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?title ?journalTitle ?volume ?number ?doi ?pub
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", ?authorName); separator=", ") AS ?authors)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", STR(?authorUri)); separator=", ") AS ?authorIds)
+WHERE{
+  VALUES ?journal {
+    <$JOURNAL>
+  }
+  ?journal dblp:primaryStreamTitle ?journalTitle .
+  ?pub dblp:publishedInStream ?journal ;
+       dblp:title ?title ;
+	   dblp:bibtexType bibtex:Article ;
+	   dblp:yearOfPublication ?year .
+  FILTER(STR(?year) = "$YEAR")
+  OPTIONAL{?pub dblp:publishedInJournalVolume ?volume}
+  OPTIONAL{?pub dblp:publishedInJournalVolumeIssue ?number}
+  OPTIONAL{?pub dblp:doi ?doi}
+  OPTIONAL {
+    ?pub dblp:hasSignature ?sig .
+    ?sig a dblp:AuthorSignature ;
+         dblp:signatureOrdinal ?ord ;
+         dblp:signatureCreator ?authorUri ;
+         dblp:signatureDblpName ?authorName .
+  }
+}
+GROUP BY ?title ?journalTitle ?volume ?number ?doi ?pub
+'''
+
+JOURNAL_OVERVIEW_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+PREFIX bibtex: <http://purl.org/net/nknouf/ns/bibtex#>
+
+SELECT ?year ?volume ?number ?journalTitle (COUNT(DISTINCT ?pub) AS ?count) WHERE {
+  VALUES ?journal {
+    <$JOURNAL>
+  }
+  ?journal dblp:primaryStreamTitle ?journalTitle .
+  ?pub dblp:publishedInStream ?journal ;
+       dblp:bibtexType bibtex:Article ;
+       dblp:yearOfPublication ?year .
+  OPTIONAL { ?pub dblp:publishedInJournalVolume ?volume }
+  OPTIONAL { ?pub dblp:publishedInJournalVolumeIssue ?number }
+}
+GROUP BY ?year ?volume ?number ?journalTitle
+ORDER BY DESC(?year) ?volume ?number
+'''
+
+# Workaround for multiple stream titles is SAMPLE(...). Don't know if there is a better way
+PERSON_TEMPLATE = '''
+PREFIX dblp: <https://dblp.org/rdf/schema#>
+
+SELECT ?title ?year ?doi ?pub ?name ?booktitle ?streamTitle ?journalVolume ?journalNumber ?stream
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", ?authorName); separator=", ") AS ?authors)
+  (GROUP_CONCAT(DISTINCT CONCAT(STR(?ord), "@@", STR(?authorUri)); separator=", ") AS ?authorIds)
+WHERE {
+  VALUES ?author {
+    <$AUTHOR>
+  }
+  ?pub dblp:authoredBy ?author ;
+       dblp:bibtexType ?bibtexType ;
+       dblp:yearOfPublication ?year ;
+       dblp:publishedInStream ?stream ;
+       dblp:title ?title .
+
+  OPTIONAL { ?pub dblp:doi ?doi }
+  OPTIONAL { ?author dblp:primaryCreatorName ?name }
+  OPTIONAL { ?pub dblp:publishedAsPartOf ?book .
+             ?book dblp:title ?booktitle }
+  OPTIONAL { ?stream dblp:primaryStreamTitle ?streamTitle }
+  OPTIONAL { ?pub dblp:publishedInJournalVolume ?journalVolume }
+  OPTIONAL { ?pub dblp:publishedInJournalVolumeIssue ?journalNumber }
+  OPTIONAL {
+    ?pub dblp:hasSignature ?sig .
+    ?sig a dblp:AuthorSignature ;
+         dblp:signatureOrdinal ?ord ;
+         dblp:signatureCreator ?authorUri ;
+         dblp:signatureDblpName ?authorName .
+  }
+}
+GROUP BY ?title ?year ?doi ?pub ?name ?booktitle ?streamTitle ?journalVolume ?journalNumber ?stream
+'''
