c48dfe9110e5f262b6d1b5a9ffca5e5458b06521
Ben Gerhards <ben.gerhards@uni-kassel.de>
2026-06-17 06:22:40 +0000

    fix regex for double quotes in title

--- database/get_data.sh
+++ database/get_data.sh
@@ -50,7 +50,7 @@ add_display_year () {
 
 
   # For each N-Triple "<pub> <predicate> "title" .", extract the 4-digit year from the title and emit a yearOfConference triple.
-  sed -En 's|^(<[^>]+>) <[^>]+> "([^"]*)([12][0-9]{3})([^"]*)".*|\1 <https://ir.webis.de/kg#yearOfConference> "\3" .|p' \
+  sed -En 's|^(<[^>]+>) <[^>]+> ".*([12][0-9]{3})[^"]*".*|\1 <https://ir.webis.de/kg#yearOfConference> "\2" .|p' \
     "$complete" > "${DATA_PATH}/chunks/yearOfConference_${stream_safe}.nt"
   rm -f "${DATA_PATH}/chunks/"*.nt.temp
 }
