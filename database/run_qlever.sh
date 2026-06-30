fc945dc2dbdf333157a20cb84c05d54f35a533e8
Ben Gerhards <ben.gerhards@uni-kassel.de>
2026-06-30 07:11:24 +0000

    run qlever index before qlever start

--- database/run_qlever.sh
+++ database/run_qlever.sh
@@ -4,7 +4,7 @@ echo $rebuild
 set -e
 if [ $rebuild = true ] ; then
     qlever get-data
-    qlever index --overwrite-existing
 else
+    qlever index --overwrite-existing
     exec qlever-server -i data/test -j 8 -p 7016 -m 10G -c 5G -e 1G -k 200 -s 30s -a $SPARQL_ACCESS_TOKEN
 fi
